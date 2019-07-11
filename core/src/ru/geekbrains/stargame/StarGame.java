24c4f02bd8ccc3c03c51b55cfba9d1c2d75bd1e5
shaitanshamma <kurbatov_andr@hotmail.com>
2019-07-11 20:54:28 +0300

    Test window 3

--- core/src/ru/geekbrains/stargame/StarGame.java
+++ core/src/ru/geekbrains/stargame/StarGame.java
@@ -1,40 +1,18 @@
 package ru.geekbrains.stargame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture background;
-	Texture background2;
+import ru.geekbrains.stargame.Screen.MenuScreen;
+
+public class StarGame extends Game {
 
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("7d9.gif");
-		background = new Texture("background.jpg");
-		background2 = new Texture("back.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		//batch.draw(background,0,0);
-		batch.draw(background2,0,0);
-		batch.draw(img, 10, 10,150f,150f);
-		batch.end();
-
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
