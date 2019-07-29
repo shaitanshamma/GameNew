913ed31b528c1a8f368c3717926a97eb116f8f3a
shaitanshamma <kurbatov_andr@hotmail.com>
2019-07-29 23:31:26 +0300

    Урок 7. Выход и гейм овер

--- core/src/ru/geekbrains/stargame/sprite/New_Game.java
+++ core/src/ru/geekbrains/stargame/sprite/New_Game.java
@@ -1,9 +1,16 @@
 package ru.geekbrains.stargame.sprite;
+
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
 import ru.geekbrains.stargame.Base.ScaledTouchUpButton;
+import ru.geekbrains.stargame.Screen.GameScreen;
+import ru.geekbrains.stargame.Screen.MenuScreen;
+import ru.geekbrains.stargame.StarGame;
 import ru.geekbrains.stargame.math.Rect;
 
 public class New_Game extends ScaledTouchUpButton {
+
 
     public New_Game(TextureAtlas atlas) {
         super(atlas.findRegion("button_new_game"));
@@ -18,6 +25,10 @@ public class New_Game extends ScaledTouchUpButton {
 
     @Override
     public void action() {
-
+      new MenuScreen(new Game() {
+            @Override
+            public void create() {
+            }
+        });
     }
 }
